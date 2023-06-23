44ea58cecbb5b14b38e9b1fb1e7fc005bc9ec3a6
hopepx <hopeparadoxsolos@gmail.com>
2023-06-23 20:20:46 +0800

    feedback push

--- feedback.js
+++ feedback.js
@@ -1,11 +1,39 @@
-// JavaScript functionality for submitting feedback/enquiry
-const form = document.querySelector('.form');
-const feedbackInput = document.getElementById("feedback-input");
-const submitButton = document.getElementById("submit-form");
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const feedback = feedbackInput.value;
-  // Send feedback to server or perform desired action here
-  alert("Thank you for your message!");
-  feedbackInput.value = "";
+const container = document.querySelector('.container');
+const emoji = document.querySelector('.emoji');
+const textarea = document.querySelector('textarea');
+const btn = document.querySelector('.btn');
+
+emoji.addEventListener('click', (e) => {
+  if (e.target.className.includes('emoji')) return;
+  textarea.classList.add('textarea--active');
+  btn.classList.add('btn--active');
+});
+
+container.addEventListener('mouseleave', () => {
+  textarea.classList.remove('textarea--active');
+  btn.classList.remove('btn--active');
+});
+
+// Get the feedback form and response message elements
+const form = document.querySelector('form');
+const responseMessage = document.querySelector('#response-message');
+
+// Add the form submit event listener
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // prevent default form submission behavior
+  
+  // Get the value of the feedback textarea
+  const feedbackText = document.querySelector('.textarea').value;
+  
+  // Check if feedback text is empty
+  if (!feedbackText) {
+    alert("Please enter your feedback or inquiry.");
+    return false;
+  }
+  
+  // Simulate receiving a response from the station manager
+  const responseText = "Thank you for your feedback. We will be addressing your concerns shortly.";
+  
+  // Set the response message content to the text from the station manager
+  responseMessage.textContent = responseText;
 });
